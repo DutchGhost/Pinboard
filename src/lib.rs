fe1d784509be89ce0c2970ef46a020bbb0532da4
DutchGhost <kasper199914@gmail.com>
2018-09-14 20:09:19 +0200

    this works

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,83 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#![cfg_attr(feature = "pinned", feature(pin))]
+
+//! This crate provides an `AsPin` trait. AsPin can be used in the same fashion as `AsRef` and `AsMut`.
+
+#[cfg(feature = "pinned")]
+pub mod implement {
+    use std::marker::Unpin;
+    use std::pin::PinMut;
+
+    /// Converts a mutable reference into a Pinned reference. See the [`pin` module].
+    ///
+    /// [`pin` module]: https://doc.rust-lang.org/nightly/std/pin/struct.PinMut.html
+    pub trait AsPin<T: ?Sized + Unpin> {
+        /// Performs the conversion.
+        fn as_pin(&mut self) -> PinMut<T>;
+    }
+
+    ///////////////////////////////////////////////
+    // GENERIC IMPL
+    ///////////////////////////////////////////////
+
+    impl<'a, T: ?Sized, U: ?Sized + Unpin> AsPin<U> for &'a mut T
+    where
+        T: AsPin<U>,
+    {
+        #[inline]
+        fn as_pin(&mut self) -> PinMut<U> {
+            (*self).as_pin()
+        }
+    }
+
+    ///////////////////////////////////////////////
+    // SLICE IMPLS
+    ///////////////////////////////////////////////
+    impl<T: Unpin> AsPin<[T]> for [T] {
+        #[inline]
+        fn as_pin(&mut self) -> PinMut<Self> {
+            PinMut::new(self)
+        }
+    }
+
+    impl AsPin<str> for str {
+        #[inline]
+        fn as_pin(&mut self) -> PinMut<Self> {
+            PinMut::new(self)
+        }
+    }
+
+    ///////////////////////////////////////////////
+    // OTHER IMPLS
+    ///////////////////////////////////////////////
+    impl<T: Unpin + ?Sized> AsPin<T> for Box<T> {
+        #[inline]
+        fn as_pin(&mut self) -> PinMut<T> {
+            PinMut::new(self)
+        }
+    }
+
+    impl<T: Unpin> AsPin<[T]> for Vec<T> {
+        #[inline]
+        fn as_pin(&mut self) -> PinMut<[T]> {
+            PinMut::new(self)
+        }
+    }
+
+    impl<T: Unpin> AsPin<Vec<T>> for Vec<T> {
+        #[inline]
+        fn as_pin(&mut self) -> PinMut<Vec<T>> {
+            PinMut::new(self)
+        }
+    }
+
+    impl AsPin<String> for String {
+        #[inline]
+        fn as_pin(&mut self) -> PinMut<String> {
+            PinMut::new(self)
+        }
     }
 }
+
+
+#[cfg(feature = "pinned")]
+pub use self::implement::AsPin;
